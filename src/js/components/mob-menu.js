0917d1757dbf1ba64aad5e45985ef9109557c300
Lusjen <lusjenh@gmail.com>
2018-06-08 19:27:59 +0300

    press about

--- src/js/components/mob-menu.js
+++ src/js/components/mob-menu.js
@@ -1,14 +1,17 @@
 var btn = $('.js-cross');
 var menuBtn = $('.menu-mobile-cross');
 var menu = $('.navbar-collapse');
+var body = $('body');
 
 btn.click(function() {
   if (!menuBtn.hasClass('is-open')) {
     menuBtn.addClass('is-open');
  		menu.addClass('is-open');
+ 		body.addClass('open');
   } else {
     menuBtn.removeClass('is-open');
  		menu.removeClass('is-open');
+ 		body.removeClass('open');
   }
    	
 });
